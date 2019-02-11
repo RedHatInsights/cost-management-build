45f3a16b7fe447fa05a2bb09eeee54b8dc79302e
Travis CI User <travis@example.org>
2019-02-11 15:46:45 +0000

    Merge pull request #468 from dlabrecq/207-table-aws
    AWS Details: Replaced PF3 ListView with PF4 table

--- da117a728fcc6e9a536f.bundle.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{672:function(n,t,a){"use strict";var c=a(0),e=a.n(c);t.a=(()=>e.a.createElement("div",null,"Not Found!"))},698:function(n,t,a){"use strict";a.r(t),function(n){var c=a(34),e=a(672);t.default=Object(c.hot)(n)(e.a)}.call(this,a(30)(n))}}]);
-//# sourceMappingURL=da117a728fcc6e9a536f.bundle.js.map
