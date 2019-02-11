c666cc565ab93a046c8f41904c6eaf4e1680fc93
Travis CI User <travis@example.org>
2019-02-11 01:47:44 +0000

    Merge pull request #466 from dlabrecq/207-table
    OCP Details: Replace PF3 ListView with PF4 table

--- e24f2e54a60894738f34.bundle.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{640:function(n,t,a){"use strict";var c=a(0),e=a.n(c);t.a=(()=>e.a.createElement("div",null,"Not Found!"))},664:function(n,t,a){"use strict";a.r(t),function(n){var c=a(32),e=a(640);t.default=Object(c.hot)(n)(e.a)}.call(this,a(28)(n))}}]);
-//# sourceMappingURL=e24f2e54a60894738f34.bundle.js.map
