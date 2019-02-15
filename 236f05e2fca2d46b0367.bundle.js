f2b597af8d15a8d0bc7cce38d1dd724ae1851cf9
Travis CI User <travis@example.org>
2019-02-15 13:07:22 +0000

    Merge pull request #491 from dlabrecq/481-more-tags
    Replace 'more tags' popover with modal

---
+++ 236f05e2fca2d46b0367.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{658:function(n,t,a){"use strict";var c=a(1),e=a.n(c);t.a=(()=>e.a.createElement("div",null,"Not Found!"))},696:function(n,t,a){"use strict";a.r(t),function(n){var c=a(51),e=a(658);t.default=Object(c.hot)(n)(e.a)}.call(this,a(50)(n))}}]);
+//# sourceMappingURL=236f05e2fca2d46b0367.bundle.js.map
