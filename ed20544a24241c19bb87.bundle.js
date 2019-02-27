326c1dda2ec37fc16d20c4d6f79a6b20be487708
Travis CI User <travis@example.org>
2019-02-27 02:31:15 +0000

    Merge pull request #534 from dlabrecq/531-victory-point
    Remove custom VictoryPoint

---
+++ ed20544a24241c19bb87.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{669:function(n,t,a){"use strict";var c=a(1),e=a.n(c);t.a=(()=>e.a.createElement("div",null,"Not Found!"))},712:function(n,t,a){"use strict";a.r(t),function(n){var c=a(52),e=a(669);t.default=Object(c.hot)(n)(e.a)}.call(this,a(51)(n))}}]);
+//# sourceMappingURL=ed20544a24241c19bb87.bundle.js.map
