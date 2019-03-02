43b1112bb04372746c4f6fe76b0ffd8c0de249da
Travis CI User <travis@example.org>
2019-03-02 22:56:06 +0000

    Fix json item to grab cost data from. (#565)

--- 86eca5bb3872ba4ea3a9.bundle.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{661:function(n,t,a){"use strict";var c=a(1),e=a.n(c);t.a=(()=>e.a.createElement("div",null,"Not Found!"))},699:function(n,t,a){"use strict";a.r(t),function(n){var c=a(21),e=a(661);t.default=Object(c.hot)(n)(e.a)}.call(this,a(20)(n))}}]);
-//# sourceMappingURL=86eca5bb3872ba4ea3a9.bundle.js.map
