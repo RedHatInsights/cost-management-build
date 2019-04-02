4c56aac09853e4c1015993c6c082fa1f3a69b172
Travis CI User <travis@example.org>
2019-04-02 13:19:35 +0000

    Merge pull request #623 from boaz0/source_details_page
    feat(source): adding the source page

---
+++ 25e93b0ba8d386919fef.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{593:function(n,t,a){"use strict";var c=a(1),e=a.n(c);t.a=(()=>e.a.createElement("div",null,"Not Found!"))},753:function(n,t,a){"use strict";a.r(t),function(n){var c=a(64),e=a(593);t.default=Object(c.hot)(n)(e.a)}.call(this,a(63)(n))}}]);
+//# sourceMappingURL=25e93b0ba8d386919fef.bundle.js.map
