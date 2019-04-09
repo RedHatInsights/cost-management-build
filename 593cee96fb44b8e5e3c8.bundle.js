b8b72b82f7f23c9334668f147ae5e9449a6a32ec
Travis CI User <travis@example.org>
2019-04-09 15:11:21 +0000

    Merge pull request #726 from dlabrecq/nav-cleanup
    Clean up router navigation properties

---
+++ 593cee96fb44b8e5e3c8.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{729:function(n,t,a){"use strict";var c=a(1),e=a.n(c);t.a=(()=>e.a.createElement("div",null,"Not Found!"))},770:function(n,t,a){"use strict";a.r(t),function(n){var c=a(53),e=a(729);t.default=Object(c.hot)(n)(e.a)}.call(this,a(52)(n))}}]);
+//# sourceMappingURL=593cee96fb44b8e5e3c8.bundle.js.map
