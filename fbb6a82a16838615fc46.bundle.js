b2681a4a0299724c123867006502e8fbe5f0711e
Travis CI User <travis@example.org>
2019-04-11 13:41:48 +0000

    Merge pull request #737 from dlabrecq/691-insights-skeleton
    Insights skeletons

--- fbb6a82a16838615fc46.bundle.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{731:function(n,t,a){"use strict";var c=a(1),e=a.n(c);t.a=(()=>e.a.createElement("div",null,"Not Found!"))},773:function(n,t,a){"use strict";a.r(t),function(n){var c=a(63),e=a(731);t.default=Object(c.hot)(n)(e.a)}.call(this,a(62)(n))}}]);
-//# sourceMappingURL=fbb6a82a16838615fc46.bundle.js.map
