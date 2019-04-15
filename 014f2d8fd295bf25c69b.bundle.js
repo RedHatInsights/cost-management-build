afa30600d5c368ca17c8d6ce2a188a83d1fd52b8
Travis CI User <travis@example.org>
2019-04-15 03:29:23 +0000

    Merge pull request #731 from boaz0/pricelist
    feat(priceList): add price list

--- 014f2d8fd295bf25c69b.bundle.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{671:function(n,t,a){"use strict";var c=a(1),e=a.n(c);t.a=(()=>e.a.createElement("div",null,"Not Found!"))},776:function(n,t,a){"use strict";a.r(t),function(n){var c=a(65),e=a(671);t.default=Object(c.hot)(n)(e.a)}.call(this,a(64)(n))}}]);
-//# sourceMappingURL=014f2d8fd295bf25c69b.bundle.js.map
