948a39773b9d8bf15294d6c826f39cc8843ac08b
Travis CI User <travis@example.org>
2019-05-29 20:18:27 +0000

    Merge pull request #879 from boaz0/closes_760
    feat(onboarding): show a notification on success

--- 3d5c2eaf4a4b4115ab82.bundle.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{800:function(n,t,a){"use strict";var c=a(1),e=a.n(c);t.a=(()=>e.a.createElement("div",null,"Not Found!"))},811:function(n,t,a){"use strict";a.r(t),function(n){var c=a(45),e=a(800);t.default=Object(c.hot)(n)(e.a)}.call(this,a(40)(n))}}]);
-//# sourceMappingURL=3d5c2eaf4a4b4115ab82.bundle.js.map
