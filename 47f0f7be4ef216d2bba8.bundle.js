42af3f147fba7a8afae8213c18b156bd76bbefb2
Travis CI User <travis@example.org>
2019-06-10 13:48:29 +0000

    Merge pull request #889 from boaz0/closes_821
    refactor(onboarding): use the clipboard copy component from pf-4

---
+++ 47f0f7be4ef216d2bba8.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{888:function(n,t,a){"use strict";var c=a(1),e=a.n(c);t.a=(()=>e.a.createElement("div",null,"Not Found!"))},901:function(n,t,a){"use strict";a.r(t),function(n){var c=a(102),e=a(888);t.default=Object(c.hot)(n)(e.a)}.call(this,a(101)(n))}}]);
+//# sourceMappingURL=47f0f7be4ef216d2bba8.bundle.js.map
