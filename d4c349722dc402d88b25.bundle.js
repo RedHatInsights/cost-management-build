7cc9db9c79488270bf4a2c10cb4c325835968237
Travis CI User <travis@example.org>
2019-08-09 09:06:21 +0000

    Merge pull request #939 from boaz0/closes_938
    fix(priceList): get price list data from costmodels endpoint

---
+++ d4c349722dc402d88b25.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{856:function(n,t,a){"use strict";var c=a(1),e=a.n(c);t.a=(()=>e.a.createElement("div",null,"Not Found!"))},911:function(n,t,a){"use strict";a.r(t),function(n){var c=a(91),e=a(856);t.default=Object(c.hot)(n)(e.a)}.call(this,a(89)(n))}}]);
+//# sourceMappingURL=d4c349722dc402d88b25.bundle.js.map
