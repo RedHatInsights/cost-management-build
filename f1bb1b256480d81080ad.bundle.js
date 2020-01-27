4dece5c3e6d5c39d61a0911118d42799bfd921dc
Travis CI User <travis@example.org>
2020-01-27 16:41:40 +0000

    Merge pull request #1270 from dlabrecq/1217-data-toolbar
    Ocp Cloud Details: PatternFly data toolbar

---
+++ f1bb1b256480d81080ad.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{1131:function(n,t,a){"use strict";var c=a(2),e=a.n(c);t.a=(()=>e.a.createElement("div",null,"Not Found!"))},1177:function(n,t,a){"use strict";a.r(t),function(n){var c=a(226),e=a(1131);t.default=Object(c.hot)(n)(e.a)}.call(this,a(225)(n))}}]);
+//# sourceMappingURL=f1bb1b256480d81080ad.bundle.js.map
