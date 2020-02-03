e5149cfc1bb48a02d0b403e58b3fb95f5b6f30d5
Travis CI User <travis@example.org>
2020-02-03 11:43:03 +0000

    Fixed yarn.lock generation with node 8 (#1301)
    Locked package.json versions for prod build / node 10
    Removed react-mirlin dependency
    Moved onboarding modal to archive dir
    Moved login session code to archive dir
    Added testURL to jest config
    Added types for node 10
    
    https://github.com/project-koku/koku-ui/issues/1300

--- 8daa68643410855bf88a.bundle.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{839:function(n,t,a){"use strict";var c=a(2),e=a.n(c);t.a=(()=>e.a.createElement("div",null,"Not Found!"))},888:function(n,t,a){"use strict";a.r(t),function(n){var c=a(255),e=a(839);t.default=Object(c.hot)(n)(e.a)}.call(this,a(254)(n))}}]);
-//# sourceMappingURL=8daa68643410855bf88a.bundle.js.map
