c8bdf3acbdf27965ea683c42def92358d38f207e
Travis CI User <travis@example.org>
2020-03-26 22:48:46 +0000

    feat(costmodels): Add cost types to cost models UI (#1431)
    Signed-off-by: Boaz Shuster <boaz.shuster.github@gmail.com>

---
+++ 7d0db0b0841ee7f58699.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{907:function(n,e,a){"use strict";a.r(e);var o=a(1),t=a.n(o);var u=()=>t.a.createElement("div",null,"Not Found!");e.default=u}}]);
+//# sourceMappingURL=7d0db0b0841ee7f58699.bundle.js.map
--- d4082c4d5d2d918ae3db.bundle.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{918:function(n,e,a){"use strict";a.r(e);var o=a(1),t=a.n(o);var u=()=>t.a.createElement("div",null,"Not Found!");e.default=u}}]);
-//# sourceMappingURL=d4082c4d5d2d918ae3db.bundle.js.map
