0b6fec5245bd56a23ff2b87b239aeb98096e1e31
Travis CI User <travis@example.org>
2020-05-18 13:51:14 +0000

    Merge pull request #1561 from dlabrecq/42-details-page-pie
    Added pie chart to details page layouts

--- 02a229c0941055fcd4d5.bundle.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{937:function(n,e,a){"use strict";a.r(e);var o=a(1),t=a.n(o);var u=()=>t.a.createElement("div",null,"Not Found!");e.default=u}}]);
-//# sourceMappingURL=02a229c0941055fcd4d5.bundle.js.map
---
+++ 97703e535f6c15381e5e.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{957:function(n,e,a){"use strict";a.r(e);var o=a(1),t=a.n(o);var u=()=>t.a.createElement("div",null,"Not Found!");e.default=u}}]);
+//# sourceMappingURL=97703e535f6c15381e5e.bundle.js.map
