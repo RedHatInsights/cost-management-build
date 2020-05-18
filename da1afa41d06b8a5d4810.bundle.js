604f8b99fc1f22e43211b77a047a49cf14f4c078
Travis CI User <travis@example.org>
2020-05-18 23:16:35 +0000

    Merge pull request #1564 from dlabrecq/42-details-page-cluster
    Added clusters widget for Ocp group_by project

--- da1afa41d06b8a5d4810.bundle.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{922:function(n,e,a){"use strict";a.r(e);var o=a(1),t=a.n(o);var u=()=>t.a.createElement("div",null,"Not Found!");e.default=u}}]);
-//# sourceMappingURL=da1afa41d06b8a5d4810.bundle.js.map
