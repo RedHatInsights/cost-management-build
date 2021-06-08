0a1941be899efb2e7c9887cfec8848c4dd71e1a3
Travis CI User <travis@example.org>
2021-06-08 22:58:32 +0000

    Merge pull request #1980 from dlabrecq/shared-modules
    Added choice to startup script for using shared PatternFly modules

---
+++ 7d518372b36359ad40bf.bundle.js
@@ -0,0 +1,2 @@
+(self.webpackChunkkoku_ui=self.webpackChunkkoku_ui||[]).push([[8446],{18446:(u,k,e)=>{var n=e(90939);u.exports=function(u,k){return n(u,k)}}}]);
+//# sourceMappingURL=7d518372b36359ad40bf.bundle.js.map
