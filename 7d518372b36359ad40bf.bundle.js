4473cb2de9e770823626b3c5bd74ec4915e5be53
Travis CI User <travis@example.org>
2021-06-09 12:57:10 +0000

    Merge pull request #1981 from dlabrecq/shared-mod
    Simplify PatternFly shared modules

--- 7d518372b36359ad40bf.bundle.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkkoku_ui=self.webpackChunkkoku_ui||[]).push([[8446],{18446:(u,k,e)=>{var n=e(90939);u.exports=function(u,k){return n(u,k)}}}]);
-//# sourceMappingURL=7d518372b36359ad40bf.bundle.js.map
